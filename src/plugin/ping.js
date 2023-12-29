b1de2bdad3187cdd0cced6460ccd9d7847efad25
Goutam Kumar <tkgoutamm625@gmail.com>
2023-12-29 21:30:31 +0530

    update src/plugin/ping.js

--- src/plugin/ping.js
+++ src/plugin/ping.js
@@ -5,8 +5,8 @@ if (m.body === ".ping") {
  const startTime = new Date();
  const { key } = await Matrix.sendMessage(m.from, { text: 'Pinging...' }, {quoted: m});
  await m.React('🍌')
- const str = `*Respond Speed: ${new Date() - startTime} ms*`
- await m.loading(str, key);
+ const text = `*Respond Speed: ${new Date() - startTime} ms*`
+ await m.typewriterEffect(text, key);
  await m.React('🍑')
 }}
 
