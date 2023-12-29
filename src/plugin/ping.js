7318100c1aaeb0e21020018652e743441b1b7466
Goutam Kumar <tkgoutamm625@gmail.com>
2023-12-29 21:29:25 +0530

    update src/plugin/ping.js

--- src/plugin/ping.js
+++ src/plugin/ping.js
@@ -3,18 +3,11 @@ const ping = async (m, Matrix) => {
 if (m.body === ".ping") {
 
  const startTime = new Date();
- const pingMsg = await Matrix.sendMessage(m.from, { text: 'Pinging...' }, {quoted: m});
+ const { key } = await Matrix.sendMessage(m.from, { text: 'Pinging...' }, {quoted: m});
  await m.React('🍌')
- await Matrix.relayMessage(m.from, {
-      protocolMessage: {
-        key: pingMsg.key,
-        type: 14,
-        editedMessage: {
-          conversation: `*Respond Speed: ${new Date() - startTime} ms*`
-        }
-      }
-  }, {});
-  await m.React('🍑')
+ const str = `*Respond Speed: ${new Date() - startTime} ms*`
+ await m.loading(str, key);
+ await m.React('🍑')
 }}
 
 export default ping;
