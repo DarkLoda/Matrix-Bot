c3170cfa9a782ee7c77e37c7a3121160abb81ead
Goutam Kumar <tkgoutamm625@gmail.com>
2023-12-30 19:44:19 +0530

    update src/plugin/AutoBio.js

--- src/plugin/AutoBio.js
+++ src/plugin/AutoBio.js
@@ -7,18 +7,18 @@ const startAutoBio = (Matrix) => {
       const status = `📆 ${new Date().toLocaleDateString()} ⌚ ${new Date().toLocaleTimeString()} Matrix ⚡`;
       Matrix.updateProfileStatus(status)
     }
-  }, 10000);
+  }, 20000);
 };
 
 const autoBio = async (m, Matrix) => {
-  if (autoBioEnabled) {
-    startAutoBio(Matrix);
+  
     //Matrix.sendMessage(Matrix.user.id, { text: 'AutoBio Activated' });
-  }
+  
   const message = m.body.toLowerCase();
 
   if (message === '.autobio on' && !autoBioEnabled) {
     autoBioEnabled = true;
+    startAutoBio(Matrix);
     await Matrix.sendMessage(m.from, { text: 'Auto bio activated!' });
   } else if (message === '.autobio off') {
     autoBioEnabled = false;
