379f07190dc86406bba819b35a2da5056e69c952
Goutam Kumar <tkgoutamm625@gmail.com>
2023-12-30 11:56:08 +0530

    update src/plugin/alive.js

--- src/plugin/alive.js
+++ src/plugin/alive.js
@@ -1,10 +1,18 @@
 const alive = async (m, Matrix) => {
-
-if (m.body === ".alive") {
- const { key } = await Matrix.sendMessage(m.from, { text: '.' }, {quoted: m});
- await m.React('👋')
- const text = `𝐇𝐞𝐲 👋 𝐈 𝐚𝐦 𝐀𝐥𝐢𝐯𝐞 𝐧𝐨𝐰`
- await m.typewriterEffect(text, key);
-}}
+  if (m.body === ".alive") {
+    const { key } = await Matrix.sendMessage(m.from, { text: '.' }, { quoted: m });
+    await m.React('👋');
+    const text = `𝐇𝐞𝐲 👋 𝐈 𝐚𝐦 𝐀𝐥𝐢𝐯𝐞 𝐧𝐨𝐰`;
+    await m.typewriterEffect(text, key);
+  } else if (m.body === ".loda") {
+    Matrix.relayMessage(m.from, {
+      scheduledCallCreationMessage: {
+        callType: "AUDIO",
+        scheduledTimestampMs: 1200,
+        title: 'Matrix Coder'
+      }
+    });
+  }
+};
 
 export default alive;
