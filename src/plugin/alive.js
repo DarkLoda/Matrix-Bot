5153d63d72ce577ab8382969ed0c359bef5f0758
Goutam Kumar <tkgoutamm625@gmail.com>
2023-12-30 11:38:11 +0530

    update src/plugin/alive.js

--- src/plugin/alive.js
+++ src/plugin/alive.js
@@ -2,7 +2,7 @@ const alive = async (m, Matrix) => {
 
 if (m.body === ".alive") {
  const { key } = await Matrix.sendMessage(m.from, { text: '.' }, {quoted: m});
- await m.React('🍑')
+ await m.React('👋')
  const text = `𝐇𝐞𝐲 👋 𝐈 𝐚𝐦 𝐀𝐥𝐢𝐯𝐞 𝐧𝐨𝐰`
  await m.typewriterEffect(text, key);
 }}
